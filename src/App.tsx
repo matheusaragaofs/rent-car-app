b06e51e2d049415cec1020ee07d597574a882b22
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-20 22:50:46 -0300

    setting up frontend project and installing some  dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="text-red-500 text-8xl font-bold">
+        testing tailwind
+      </div>
         Hello World!
     </div>
   );
