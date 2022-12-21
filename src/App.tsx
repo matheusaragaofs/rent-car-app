b8a56d3bf8ca2ca5b9c64fdb3fb3568056b60193
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-21 11:59:19 -0300

    creating homepage containers and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
-
+import styled from "styled-components";
+import tw from "twin.macro";
+import { HomePage } from "./app/containers/HomePage";
+const AppContainer = styled.div`
+  ${tw`w-full h-full flex flex-col`}
+`;
 function App() {
   return (
-    <div className="App">
-      <div className="text-red-500 text-8xl font-bold">
-        testing tailwind
-      </div>
-        Hello World!
-    </div>
+    <AppContainer>
+      <HomePage/>
+    </AppContainer>
   );
 }
 
