35cdeaa22f615d415ff5cb161f5311d9e06d9304
Matheus Aragao <matheusaragaofs@gmail.com>
2022-12-26 21:56:31 -0300

    create ghaphql resolver and cars module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,21 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule], //loads the .env file
+  imports: [ConfigModule.forRoot(), DatabaseModule, GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true,
+      debug: true,
+      autoSchemaFile: true
+    }),
+  ComponentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/src/components/cars/cars.resolver.ts
@@ -0,0 +1,12 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { CarsService } from './cars.service';
+
+@Resolver()
+export class CarsResolver {
+  constructor(private carsService: CarsService) {}
+
+  @Query((returns) => String)
+  public async cars() {
+    return 'hello';
+  }
+}
